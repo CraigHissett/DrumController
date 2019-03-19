8aad29acdc371b64f6f15977e67e2a8753e5461c
Craig Hissett <CraigHissett@users.noreply.github.com>
2019-03-19 00:33:15 +0000

    Split code compiling- test

--- Main/MIDIHandler/MIDIHandler.ino
+++ Main/MIDIHandler/MIDIHandler.ino
@@ -1,1 +1,40 @@
 /*Code to process MIDI commands using data received from SPI Slaves*/
+
+#include <midi_serialization.h>
+#include <usbmidi.h>
+#include <Wire.h>
+
+const int SLAVE_ADDRESS = 1; //Slave arduino ID
+int table[]={0,0}; //the data will be transmited via table as to allow different data to be transfer.
+
+void setup()
+{
+  Wire.begin();   
+}
+
+void loop()
+{
+    while (USBMIDI.available()) {
+    // We must read entire available data, so in case we receive incoming
+    // MIDI data, the host wouldn't get stuck.
+    u8 b = USBMIDI.read();
+}
+  Wire.requestFrom(SLAVE_ADDRESS, 2);     // request 2 bytes from slave device #2
+  for(int i=0;i<3;i++)//organizes the data from the slave in the table
+  {
+    int c = Wire.read(); // receive a byte as character
+  table[i]=c;
+  }
+
+  if(table[0] != 0){
+    sendNote(0, table[0], table[1]);
+  }
+  // Flush the output.
+  USBMIDI.flush();
+}
+
+void sendNote(uint8_t channel, uint8_t note, uint8_t velocity) {
+  USBMIDI.write((velocity != 0 ? 0x90 : 0x80) | (channel & 0xf));
+  USBMIDI.write(note & 0x7f);
+  USBMIDI.write(velocity &0x7f);
+}
--- Main/PadHandler/PadHandler.ino
+++ Main/PadHandler/PadHandler.ino
@@ -2,6 +2,7 @@
 
 #include <midi_serialization.h>
 #include <usbmidi.h>
+#include <Wire.h>
 
 // general midi drum notes
 #define note_bassdrum     35
@@ -10,16 +11,15 @@
 #define note_hihatopen    44
 #define note_crash        49
 
-void sendCC(uint8_t channel, uint8_t control, uint8_t value) {
-  USBMIDI.write(0xB0 | (channel & 0xf));
-  USBMIDI.write(control & 0x7f);
-  USBMIDI.write(value & 0x7f);
-}
+const int SLAVE_ADDRESS = 1; //Slave arduino ID
+int table[]={0,0};
 
-void sendNote(uint8_t channel, uint8_t note, uint8_t velocity) {
-  USBMIDI.write((velocity != 0 ? 0x90 : 0x80) | (channel & 0xf));
-  USBMIDI.write(note & 0x7f);
-  USBMIDI.write(velocity &0x7f);
+void requestEvent()
+{
+  uint8_t Buffer[2];
+  Buffer[0]=table[0];
+  Buffer[1]=table[1];
+  Wire.write(Buffer,2);
 }
 
 const int ANALOG_PIN_COUNT = 16;
@@ -28,7 +28,7 @@ const int BUTTON_PIN_COUNT = 5;
 // Change the order of the pins to change the ctrl or note order.
 int analogPins[ANALOG_PIN_COUNT] = { A15, A14, A13, A12, A11, A10, A9, A8, A7, A6, A5, A4, A3, A2, A1, A0 };
 int buttonPins[BUTTON_PIN_COUNT] = { 9, 8, 7, 6, 5 };
-int DrumNote[ANALOG_PIN_COUNT] = { note_bassdrum, note_snaredrum, note_hihatclosed note_hihatopen, note_crash, note_bassdrum, note_snaredrum, note_hihatclosed note_hihatopen, note_crash, note_bassdrum, note_snaredrum, note_hihatclosed note_hihatopen, note_crash, note_bassdrum };
+int DrumNote[ANALOG_PIN_COUNT] = { note_bassdrum, note_snaredrum, note_hihatclosed, note_hihatopen, note_crash, note_bassdrum, note_snaredrum, note_hihatclosed, note_hihatopen, note_crash, note_bassdrum, note_snaredrum, note_hihatclosed, note_hihatopen, note_crash, note_bassdrum };
 
 int ccValues[ANALOG_PIN_COUNT];
 int buttonDown[BUTTON_PIN_COUNT];
@@ -38,51 +38,29 @@ int readCC(int pin) {
   return analogRead(pin) >> 3;
 }
 
-int isButtonDown(int pin) {
-  return digitalRead(pin) == 0;
-}
-
 void setup() {
+Wire.begin(SLAVE_ADDRESS);      // join i2c bus with address #1
+Wire.onRequest(requestEvent);   // register event
+//Wire.onReceive(receiveEvent); // register event
+
   // Initialize initial values.
   for (int i=0; i<ANALOG_PIN_COUNT; ++i) {
     pinMode(analogPins[i], INPUT);
     ccValues[i] = readCC(analogPins[i]);
   }
-
-  for (int i=0; i<BUTTON_PIN_COUNT; ++i) {
-    pinMode(buttonPins[i], INPUT);
-    digitalWrite(buttonPins[i], HIGH);
-    buttonDown[i] = isButtonDown(buttonPins[i]);
-  }
 }
 
 void loop() {
-  while (USBMIDI.available()) {
-    // We must read entire available data, so in case we receive incoming
-    // MIDI data, the host wouldn't get stuck.
-    u8 b = USBMIDI.read();
-  }
-
   for (int i=0; i<ANALOG_PIN_COUNT; ++i) {
     int value = readCC(analogPins[i]);
 
     // Send CC only if th has changed.
     if (ccValues[i] != value) {
       //sendCC(0, i, value);
-      sendNote(0, 64 + i, value);
+      //sendNote(0, DrumNote[i], value);
+      table[0]=DrumNote[i];
+      table[1]=value;
       ccValues[i] = value;
     }
   }
-
-  for (int i=0; i<BUTTON_PIN_COUNT; ++i) {
-    int down = isButtonDown(buttonPins[i]);
-
-    if (down != buttonDown[i]) {
-      sendNote(0, 64 + i, down ? 127 : 0);
-      buttonDown[i] = down;
-    }
-  }
-
-  // Flush the output.
-  USBMIDI.flush();
 }
